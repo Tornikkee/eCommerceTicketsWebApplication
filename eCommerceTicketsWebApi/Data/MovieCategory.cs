ed3ae7579a8f3d68854a698256490e3ee5b16392
Tornike Khamitsashvili <xemenwk@gmail.com>
2022-07-25 15:45:34 +0400

    Add project files.

---
+++ eCommerceTicketsWebApi/Data/MovieCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace eCommerceTicketsWebApi.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Biography,
+        Fantasy,
+        Melodrama,
+        Cartoon,
+        Horror
+    }
+}
