65d66b63756a31b2b54417812e9fe17f007ce197
Tornike Khamitsashvili <xemenwk@gmail.com>
2022-07-30 12:56:47 +0400

    Updated

--- eCommerceTicketsWebApi/Controllers/CinemasController.cs
+++ eCommerceTicketsWebApi/Controllers/CinemasController.cs
@@ -1,4 +1,5 @@
 ﻿using eCommerceTicketsWebApi.Data;
+using eCommerceTicketsWebApi.Models;
 using eCommerceTicketsWebApplication.Data.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,19 @@ namespace eCommerceTicketsWebApplication.Controllers
         {
             var allCinemas = await _service.GetAllAsync();
             return View(allCinemas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Logo,Name,Description")]Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await _service.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- eCommerceTicketsWebApi/Models/Cinema.cs
+++ eCommerceTicketsWebApi/Models/Cinema.cs
@@ -5,16 +5,24 @@ namespace eCommerceTicketsWebApi.Models
 {
     public class Cinema : IEntityBase
     {
+        public Cinema()
+        {
+            Movies = new List<Movie>();
+        }
+
         [Key]
         public int Id { get; set; }
 
         [Display(Name = "Cinema Logo")]
+        [Required(ErrorMessage = "Cinema logo is required")]
         public string Logo { get; set; }
 
         [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Cinema name is required")]
         public string Name { get; set; }
 
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Cinema description is required")]
         public string Description { get; set; }
 
         //RelationProps
