8f5c2270c8fa307a2f839fea984767072a54bfa8
Tornike Khamitsashvili <xemenwk@gmail.com>
2022-07-31 21:19:24 +0400

    UpdatedProject

---
+++ eCommerceTicketsWebApi/Data/ViewModels/NewMovieDropDownsVM.cs
@@ -0,0 +1,15 @@
+﻿using eCommerceTicketsWebApi.Models;
+
+namespace eCommerceTicketsWebApplication.Data.ViewModels
+{
+    public class NewMovieDropDownsVM
+    {
+        public NewMovieDropDownsVM()
+        {
+
+        }
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actor> Actors { get; set; }
+    }
+}
