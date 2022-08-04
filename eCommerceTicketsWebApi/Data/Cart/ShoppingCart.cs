c225b6fa59129f6859acff3fd12df0fb6c03a086
Tornike Khamitsashvili <xemenwk@gmail.com>
2022-08-04 17:33:40 +0400

    Updated

--- eCommerceTicketsWebApi/Data/Cart/ShoppingCart.cs
+++ eCommerceTicketsWebApi/Data/Cart/ShoppingCart.cs
@@ -80,5 +80,12 @@ namespace eCommerceTicketsWebApplication.Data.Cart
             var total = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
             return total;
         }
+
+        public async Task ClearShoppingCartAsync()
+        {
+            var items = await _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).ToListAsync();
+            _context.ShoppingCartItems.RemoveRange(items);
+            await _context.SaveChangesAsync();
+        }
     }
 }
---
+++ eCommerceTicketsWebApi/Data/Services/IOrdersService.cs
@@ -0,0 +1,11 @@
+﻿using eCommerceTicketsWebApplication.Models;
+
+namespace eCommerceTicketsWebApplication.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+
+        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+    }
+}
