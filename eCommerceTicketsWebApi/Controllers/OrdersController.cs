c1b1e71c19be4c0e17cdb09fab2c4ca8bf87f611
Tornike Khamitsashvili <xemenwk@gmail.com>
2022-08-04 14:00:34 +0400

    Updated

--- eCommerceTicketsWebApi/Controllers/OrdersController.cs
+++ eCommerceTicketsWebApi/Controllers/OrdersController.cs
@@ -28,5 +28,16 @@ namespace eCommerceTicketsWebApplication.Controllers
 
             return View(response);
         }
+
+        public async Task<RedirectToActionResult> AddToShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if(item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
     }
 }
